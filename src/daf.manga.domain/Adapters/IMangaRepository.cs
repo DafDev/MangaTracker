d16587625fb3e32cb95a60f0485374cb97031540
Daf <dafDev@gmail.com>
2026-03-28 22:45:42 +0100

    feat: add cancellation token to manga repository methods

--- src/daf.manga.domain/Adapters/IMangaRepository.cs
+++ src/daf.manga.domain/Adapters/IMangaRepository.cs
@@ -2,9 +2,9 @@ namespace Daf.Manga.Domain.Adapters;
 using System.Collections.Generic;
 public interface IMangaRepository
 {
-    Task<IEnumerable<Manga>> GetAllManga();
-    Task<Manga> GetMangaByTitle(string title);
-    Task AddManga(Manga manga);
-    Task UpdateManga(Manga manga);
-    Task DeleteManga(string title);
+    Task<IEnumerable<Manga>> GetAllManga(CancellationToken cancellationToken);
+    Task<Manga> GetMangaByTitle(string title, CancellationToken cancellationToken);
+    Task AddManga(Manga manga, CancellationToken cancellationToken);
+    Task UpdateManga(Manga manga, CancellationToken cancellationToken);
+    Task DeleteManga(string title, CancellationToken cancellationToken);
 }
