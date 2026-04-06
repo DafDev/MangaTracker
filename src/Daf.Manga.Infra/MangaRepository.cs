db0cd3815d3569de6da4f1b36edc3ff0aea53543
Daf <dafDev@gmail.com>
2026-04-06 15:35:24 +0200

    feat(infrà: start implementing infra

---
+++ src/Daf.Manga.Infra/MangaRepository.cs
@@ -0,0 +1,33 @@
+﻿using Daf.Manga.Domain.Adapters;
+using LightResults;
+using Microsoft.Extensions.Logging;
+
+namespace Daf.Manga.Infra;
+
+public class MangaRepository(ILogger<MangaRepository> logger) : IMangaRepository
+{
+    public Task<Result<IEnumerable<Domain.Manga>>> GetAllManga(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<Domain.Manga>> GetMangaByTitle(string title, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> AddManga(Domain.Manga manga, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> UpdateManga(Domain.Manga manga, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result> DeleteManga(string title, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Daf.Manga.Infra/ServicesCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Daf.Manga.Domain.Adapters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Daf.Manga.Infra;
+
+public static class ServicesCollectionExtensions
+{
+    public static IServiceCollection AddMangaApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IMangaRepository, MangaRepository>();
+        return services;
+    }
+}
