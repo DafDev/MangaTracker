a5ed55cf00a5bc39f6e82dc4057c84bf6e9cdd02
Daf <dafDev@gmail.com>
2026-04-07 04:02:30 +0200

    feat(api): added first api routes

---
+++ src/Daf.Manga.Api/Endpoints/MangaEndpoints.cs
@@ -0,0 +1,43 @@
+﻿using Daf.Manga.Domain.Adapters;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Daf.Manga.Api.Endpoints;
+
+public static class MangaEndpoints
+{
+    private const string Base = "api/manga";
+
+    public static void MapMangaEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var mangaApi = routes.MapGroup(Base).WithTags("Manga");
+        mangaApi.MapGet("/", GetAllMangas);
+        mangaApi.MapPost("/", AddManga);
+        
+        mangaApi.MapGet("/{title}", GetMangaByTitle);
+
+    }
+    
+    private static async Task<Results<Ok<IAsyncEnumerable<Application.Dto.Manga>>, NotFound>> GetAllMangas(IMangaService service, CancellationToken cancellationToken)
+    {
+        var mangasResult = await service.GetAllManga(cancellationToken);
+        return mangasResult.IsSuccess(out var mangas)
+            ? TypedResults.Ok(mangas.Select(Application.Dto.Manga.FromDomain)) 
+            : TypedResults.NotFound();
+    }
+    
+    private static async Task<Results<Created, NotFound>> AddManga(IMangaService service, Application.Dto.Manga manga, CancellationToken cancellationToken)
+    {
+        var mangasResult = await service.AddManga(manga.ToDomain(), cancellationToken);
+        return mangasResult.IsSuccess()
+            ? TypedResults.Created()
+            : TypedResults.NotFound();
+    }
+    
+    private static async Task<Results<Ok<Application.Dto.Manga>, NotFound>> GetMangaByTitle(IMangaService service, string title,CancellationToken cancellationToken)
+    {
+        var mangasResult = await service.GetMangaByTitle(title, cancellationToken);
+        return mangasResult.IsSuccess(out var manga)
+            ? TypedResults.Ok(Application.Dto.Manga.FromDomain(manga)) 
+            : TypedResults.NotFound();
+    }
+}
--- src/Daf.Manga.Api/Program.cs
+++ src/Daf.Manga.Api/Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Daf.Manga.Application;
+using Daf.Manga.Infra;
+using Scalar.AspNetCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddMangaInfrastructure(builder.Configuration);
+builder.Services.AddMangaApplication();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
 app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
 
 app.Run();
